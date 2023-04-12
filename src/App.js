e85412ee98b1623f8802d54aa9778cb008e625aa
EliasBerhe <eliastes123@gmial.com>
2023-04-11 23:46:57 -0400

    update component folder placement

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import './index.css'
-import QueryPlanet from './assets/components/QueryPlanet';
-import HomePage from './assets/components/HomePage';
-import Search from './assets/components/Search';
+import QueryPlanet from './components/QueryPlanet';
+import HomePage from './components/HomePage';
+import Search from './components/Search';
 function App() {
   return (
     <BrowserRouter>
